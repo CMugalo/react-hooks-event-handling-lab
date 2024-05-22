f8e7b560564c9b535746bb7365bd6d85f745a848
Christian Mugalo <christian.mugalo@student.moringaschool.com>
2024-05-22 07:30:02 +0300

    adds event handlers

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,15 @@
 // Code Keypad Component Here
+import React from "react";
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  function handlePassword() {
+    console.log("Entering password...");
+  }
+  return (
+    <div>
+      <input type="password" onChange={handlePassword}></input>
+    </div>
+  );
 }
 
 export default Keypad;
